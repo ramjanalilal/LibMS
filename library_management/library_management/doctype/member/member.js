756a390b7b21d645306e47e249ed727eaa4c2fc8
ramjanalilal <ramjanalilal143@gmail.com>
2024-09-07 04:53:43 +0000

    When you create a member with mobie number and Email to create auto contact and Address

--- library_management/library_management/doctype/member/member.js
+++ library_management/library_management/doctype/member/member.js
@@ -1,107 +1,14 @@
 // Copyright (c) 2023, ramjanali and contributors
 // For license information, please see license.txt
 
-
-//frappe.ui.form.on("Member", "member_contact", function(frm, cdt, cdn) {
-//			return {
-//				filters: {
-//					'link_doctype': 'Member',
-//					'link_name': doc.name
-//				}
-//			}
-//		})
-//		frm.set_query('member_contact', function(doc) {
-//			return {
-//				filters: {
-//					'link_doctype': 'Member',
-//					'link_name': doc.name
-//				}
-//			}
-//		})
-//	}																	
-
-// frappe.ui.form.on('Member', {
-// 	refresh(frm) {
-// 		frm.set_query('member_address', function(doc) {
-// 			return {
-// 				filters: {
-// 					'link_doctype': 'Member',
-// 					'link_name': doc.name
-// 				}
-// 			}
-// 		})
-// 		frm.set_query('member_contact', function(doc) {
-// 			return {
-// 				filters: {
-// 					'link_doctype': 'Member',
-// 					'link_name': doc.name
-// 				}
-// 			}
-// 		})
-// 	},
-// 	verify:function(frm){
-// 		var otp = generateOTP();
-//         //frm.set_value('otp', otp);
-//         frappe.msgprint(__("OTP generated successfully: ") + otp);
-// 		let d = new frappe.ui.Dialog({
-// 			fields: [
-// 				{
-// 					label: 'OTP',
-// 					fieldname: 'verify_otp',
-// 					fieldtype: 'Data'
-// 				}
-// 			],
-// 			primary_action_label: 'Save',
-// 			primary_action(values) {
-// 				verify_mobile(frm, values.verify_otp); // Pass the entered OTP to the function
-// 				d.hide();
-// 			}
-// 		});
-// 		d.show();
-// 		function verify_mobile(frm, enteredOTP) {
-// 			var generatedOTP = frm.doc.verify_otp;
-// 			if (generatedOTP === enteredOTP) {
-// 				frappe.msgprint(__('Verify Member'));
-// 			} else {
-// 				frappe.msgprint(__('Enter Valid OTP'));
-// 			}
-// 		}
-// 	}
-// });
-
-
-
-// function generateOTP() {
-//     var otp = '';
-//     var possibleDigits = '0123456789';
-
-//     for (var i = 0; i < 6; i++) {
-//         otp += possibleDigits.charAt(Math.floor(Math.random() * possibleDigits.length));
-//     }
-//     return otp;
-// }
-
 frappe.ui.form.on('Member', {
     refresh: function(frm) {
-        // Add "Verify" button to the toolbar
-        // frm.add_custom_button(__('Verify'), function() {
-        //     generateAndVerifyOTP(frm);
-        // });
+
     },
+    
     verify: function(frm) {
         generateAndVerifyOTP(frm);
     },
-    // status_check: function(frm) {
-    //     frappe.call({
-    //         method: "status_update_from_table",
-    //         args: {
-    //             member: frm.doc.name
-    //         },
-    //         callback: function(data) {
-    //             // console.log(data);
-    //         }
-    //     });
-    // },
     // status_check: function(frm) {
     //     frappe.call({
     //         method: "status_update_from_table",
@@ -115,8 +22,6 @@ frappe.ui.form.on('Member', {
     //         }
     //     });
     // }
-    
-
 });
 
 
@@ -163,3 +68,34 @@ function generateOTP() {
     return otp;
 }
 
+
+frappe.ui.form.on('Member', {
+	refresh: function(frm) {
+
+		frappe.dynamic_link = {doc: frm.doc, fieldname: 'name', doctype: 'Member'};
+
+		frm.toggle_display(['address_html','contact_html'], !frm.doc.__islocal);
+
+		if(!frm.doc.__islocal) {
+			frappe.contacts.render_address_and_contact(frm);
+			erpnext.utils.set_party_dashboard_indicators(frm);
+
+		} else {
+			frappe.contacts.clear_address_and_contact(frm);
+		}
+        if (!frm.doc.customer) {
+            frm.add_custom_button(__('Create Customer'), () => {
+                frm.call('make_customer_and_link').then(() => {
+                    frm.reload_doc();
+                });
+            });
+        }
+	},
+    after_save: function(frm) {
+        if (!frm.doc.customer) {
+            frm.call('make_customer_and_link').then(() => {
+                frm.reload_doc();
+            });
+        }
+    }
+});
--- library_management/library_management/doctype/member/member.py
+++ library_management/library_management/doctype/member/member.py
@@ -1,25 +1,113 @@
 import frappe
+from frappe import _
+from frappe.contacts.address_and_contact import load_address_and_contact
 from frappe.model.document import Document
-from frappe.contacts.address_and_contact import (
-	delete_contact_and_address,
-	load_address_and_contact,
-)
+from frappe.utils import get_link_to_form
 
 class Member(Document):
+    def onload(self):
+        """Load address and contacts in `__onload`"""
+        load_address_and_contact(self)
+
+    def validate_email_type(self, email):
+        from frappe.utils import validate_email_address
+        validate_email_address(email.strip(), True)
+
     def validate(self):
         self.member_name = f'{self.first_name} {self.middle_name or ""} {self.last_name or ""}'
-        self.create_customer_from_member()
+        if self.email_id:
+            self.validate_email_type(self.email_id)
+            
+    @frappe.whitelist()
+    def make_customer_and_link(self):
+        if self.customer:
+            frappe.msgprint(_("A customer is already linked to this Member"))
+            return
 
-    def create_customer_from_member(self):
-        if self.is_new():
-            customer_doc = frappe.get_doc({
-                'doctype': 'Customer',
-                'customer': self.name,
-                'customer_name': self.member_name
-                
+        customer = create_customer(frappe._dict({
+            'fullname': self.member_name,
+            'email': self.email_id,
+            'phone': None,
+            'mobile': self.mobile,
+            'member': self.name,
+        }))
+
+        self.customer = customer
+        self.save()
+        frappe.msgprint(_("Customer {0} has been created successfully.").format(self.customer))
+
+
+def get_or_create_member(user_details):
+    member_list = frappe.get_all("Member", filters={'email': user_details.email, 'membership_type': user_details.plan_id})
+    if member_list:
+        frappe.msgprint()
+        return member_list[0]['name']
+    else:
+        return create_member(user_details)
+
+def create_member(user_details):
+    user_details = frappe._dict(user_details)
+    member = frappe.new_doc("Member")
+    member.update({
+        "member_name": user_details.fullname,
+        "email_id": user_details.email,
+        "pan_number": user_details.pan or None,
+        "membership_type": user_details.plan_id,
+        "customer_id": user_details.customer_id or None,
+        "subscription_id": user_details.subscription_id or None,
+        "subscription_status": user_details.subscription_status or ""
+    })
+
+    member.insert(ignore_permissions=True)
+    member.customer = create_customer(user_details, member.name)
+    member.save(ignore_permissions=True)
+
+    return member
+
+def create_customer(user_details, member=None):
+    frappe.msgprint(f"Details: {user_details}")
+    member = user_details.member
+    customer = frappe.new_doc("Customer")
+    customer.customer_name = user_details.fullname
+    customer.member = member
+    customer.customer_type = "Individual"
+    customer.customer_group = frappe.db.get_single_value("Library Setting", "customer_group")
+    customer.territory = frappe.db.get_single_value("Selling Settings", "territory")
+    customer.flags.ignore_mandatory = True
+    customer.insert(ignore_permissions=True)
+
+    try:
+        frappe.db.savepoint("contact_creation")
+        contact = frappe.new_doc("Contact")
+        contact.first_name = user_details.fullname
+        if user_details.mobile:
+            contact.add_phone(user_details.mobile, is_primary_phone=0, is_primary_mobile_no=1)
+        if user_details.email:
+            contact.add_email(user_details.email, is_primary=1)
+        contact.insert(ignore_permissions=True)
+
+        contact.append("links", {
+            "link_doctype": "Customer",
+            "link_name": customer.name
+        })
+
+        if member:
+            frappe.msgprint(f"Appending Member link")
+            contact.append("links", {
+                "link_doctype": "Member",
+                "link_name": member
             })
-            frappe.msgprint("Customer created successfully")
-            customer_doc.insert()
+
+        contact.save(ignore_permissions=True)
+
+    except frappe.DuplicateEntryError:
+        return customer.name
+
+    except Exception as e:
+        frappe.db.rollback(save_point="contact_creation")
+        frappe.log_error(frappe.get_traceback(), _("Contact Creation Failed"))
+
+    return customer.name
 
 @frappe.whitelist()
 def update_all_members_status():
@@ -38,5 +126,3 @@ def update_all_members_status():
             frappe.msgprint(f"Member {member['name']} status updated to Expired.")
         else:
             frappe.msgprint(f"Member {member['name']} has active memberships.")
-
-
